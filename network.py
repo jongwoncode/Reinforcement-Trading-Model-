66cf9c5c316df3574868b399cfd6f8ace7df9e65
jongwon2 <uj02030@naver.com>
2022-12-31 21:53:19 +0900

    [UPDATE] Network 구조 단순화

--- network.py
+++ network.py
@@ -1,15 +1,7 @@
-import os
-import time
-import threading
-import random
 import numpy as np
 import tensorflow as tf
-
-from tensorflow.compat.v1.train import AdamOptimizer
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import LSTM, Dense, Input, Concatenate
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
 
 
 # ActorCritic 인공신경망
@@ -23,19 +15,16 @@ class LSTM_DNN_AC(tf.keras.Model) :
 
         # chart_state : (n_steps, chart_columns) ex) (5, 28)
         self.lstm1 = LSTM(128, activation='tanh', dropout= 0.3, return_sequences=True)
-        self.lstm2 = LSTM(64, activation = 'tanh', dropout = 0.3, return_sequences=True)
-        self.lstm3 = LSTM(16, activation = 'tanh', return_sequences=False)
+        self.lstm2 = LSTM(16, activation = 'tanh', return_sequences=False)
         # balance_state : (balance_info) ex) (4)
-        self.dnn1 = Dense(64, activation='relu')
+        self.dnn1 = Dense(32, activation='relu')
         self.dnn2 = Dense(16, activation='relu')
         # concatenate & shared network
         self.concatenate = Concatenate()
-        self.shared_fc = Dense(128, activation='relu')
+        self.shared_fc = Dense(64, activation='relu')
         # Actor part
-        self.poilcy1 = Dense(32, activation='relu')
         self.policy = Dense(action_size, activation='linear')
         # Critic part
-        self.value1 = Dense(32, activation='relu')
         self.value = Dense(1, activation='linear')
     
     def call(self, inputs) :
@@ -43,7 +32,6 @@ class LSTM_DNN_AC(tf.keras.Model) :
         c_inp = inputs[0]
         c = self.lstm1(c_inp)
         c = self.lstm2(c)
-        c = self.lstm3(c)
         # DNN PART
         b_inp = inputs[1]
         b = self.dnn1(b_inp)
@@ -51,12 +39,9 @@ class LSTM_DNN_AC(tf.keras.Model) :
         # CONCATENATE & SHARED PART -> (...chart, ...balance) (16+16, )
         shared = self.concatenate([c, b])
         shared = self.shared_fc(shared)
-        # ACTOR PART
-        policy = self.poilcy1(shared)
-        policy = self.policy(policy)
-        # CRITIC PART
-        value = self.value1(shared)
-        value = self.value(value)
+        # ACTOR PART, CRITIC PART
+        policy = self.policy(shared)
+        value = self.value(shared)
         return policy, value
 
     # Subclassing API에서 plot_model을 그리기 위해서 build_graph 메서드 생성
@@ -64,4 +49,3 @@ class LSTM_DNN_AC(tf.keras.Model) :
         c_inp = Input(shape= (self.n_steps, self.state_size), dtype=tf.float32)
         b_inp = Input(shape= (self.balance_size, ), dtype=tf.float32)
         return Model(inputs=[c_inp, b_inp], outputs=self.call([c_inp, b_inp]))
-
