7dd14ad488a35cec5ca643575dc8b565114a0bc9
jongwon2 <uj02030@naver.com>
2023-01-08 21:28:00 +0900

    [UPDATE] softmax 함수 tensorflow로 수정

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import time
 import datetime
 import os
 import numpy as np
+import tensorflow as tf
 
 BASE_DIR = os.getcwd()
 LOGGER_NAME = 'rltrader'
@@ -12,8 +13,8 @@ def get_time_str():
         int(time.time())).strftime(FORMAT_DATETIME)
 
 def softmax(x) : 
-    c = np.max(x) 
-    exp_a = np.exp(x-c) 
-    sum_exp_a = np.sum(exp_a)
+    c = tf.reduce_max(x) 
+    exp_a = tf.math.exp(x-c) 
+    sum_exp_a = tf.reduce_sum(exp_a)
     y = exp_a / sum_exp_a
     return y
